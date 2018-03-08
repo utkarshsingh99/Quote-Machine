25528cd903ef37b64ba112234ab9f5307fe9d83b
utkarshsingh99 <utkarshsingh369@gmail.com>
2018-03-08 23:35:24 +0530

    Added Text Animation

--- app/src/main/java/com/perfection/utkarsh/quoteaquote/MainActivity.java
+++ app/src/main/java/com/perfection/utkarsh/quoteaquote/MainActivity.java
@@ -2,6 +2,7 @@ package com.perfection.utkarsh.quoteaquote;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 
@@ -11,7 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-        TextView text =(TextView)findViewById(R.id.intro);
-        text.startAnimation(AnimationUtils.loadAnimation(this, android.R.anim.fade_in));
+        TextView text1 = findViewById(R.id.intro1);
+        TextView text2 = findViewById(R.id.intro2);
+        TextView text3 = findViewById(R.id.intro3);
+        Animation animation;
+        animation = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade_in);
+        text1.startAnimation(animation);
+        text2.startAnimation(animation);
+        text3.startAnimation(animation);
     }
 }
